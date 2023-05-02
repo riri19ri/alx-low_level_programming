9baf33fafa7c68289af02fc27efbad06f72cc232
riri19ri <121935990+riri19ri@users.noreply.github.com>
2023-05-02 11:04:05 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-	#include <stdlib.h>
-
-
+#include <stdlib.h>
 	/**
 	 * main - prints its own opcodes
 	 * @argc: number of arguments
 	 * @argv: array of arguments
-	 *
 	 * Return: Always 0 (Success)
 	 */
 	int main(int argc, char *argv[])
